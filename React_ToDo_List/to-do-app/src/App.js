b081906524f5be602c8ca61ae24201fa22237b31
Jangbeen (J.B.) Park <pab2060@gmail.com>
2023-01-21 19:33:53 -0700

    React To Do List w/o CSS

---
+++ React_ToDo_List/to-do-app/src/App.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react"
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    // This prevent form tag's submit event which is default event
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDos((currentArr) => [toDo, ...currentArr]) // Spread Opreator to expand elements in an array
+    setToDo("");  // When submoit button clicked reset input box
+    console.log("h")
+  }
+
+  const deleteClick = index  => {
+    setToDos((currentArr) => currentArr.filter((_,curIndex) => curIndex !== index))
+  }
+  return (
+    <div>
+      <h1>To Do ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+      <input 
+        value={toDo} 
+        onChange={onChange} 
+        type="text" 
+        placeholder='Write your to do' />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) =>
+          <li key={index}> {item} <button onClick={() => deleteClick(index)}>Delete</button></li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
