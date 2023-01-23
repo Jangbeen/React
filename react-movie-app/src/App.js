7e3e9d790784f419ed37944a2d4ecc5b4edd1a7c
Jangbeen (J.B.) Park <pab2060@gmail.com>
2023-01-22 17:27:16 -0700

    React Router

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,16 +1,22 @@
-import Button from "./Button";
-import styles from "./App.module.css"
-import { useState } from "react";
-function App() {
-  const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter((current) => current + 1)
+// import { useState, useEffect } from "react";
+// import Movie from "./componenets/Movie";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
 
+function App() {
   return (
-    <div>
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<About />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
